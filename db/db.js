5c520b07580114e9ff39cf082c4a85c612ddddc7
Md Ishtiyaque Ahmad <mda.ug19.cs@nitp.ac.in>
2025-11-29 22:45:29 +0530

    Update skills, add new CSS styles, and enhance project section in portfolio

--- db/db.js
+++ db/db.js
@@ -1,115 +1,142 @@
 export const skills = {
   language: [
-    {   type: 'img',
+    {
+      //type: 'img',
+      title: "Java",
+      icon: "fab fa-java",
+      color: "#9296dbff",
+    },
+    {
+      type: "img",
+      title: "C#/.NET",
+      icon: "images/Logo_C_sharp.svg.png",
+      color: "#e43e00",
+    },
+    {
+      title: "Python",
+      icon: "fab fa-python",
+      color: "#f3c623",
+    },
+    {
+      title: "JavaScript",
+      icon: "fab fa-js-square",
+      color: "#b2b200",
+    },
+    {
+      type: "img",
 
-      title: 'Cpp',
-      icon: 'images/cpp_logo.svg',
-      color: '#e43e00'
+      title: "Cpp",
+      icon: "images/cpp_logo.svg",
+      color: "#e43e00",
     },
     {
-      title: 'Python',
-      icon: 'fab fa-python',
-      color: '#f3c623'
+      title: "HTML",
+      icon: "fab fa-html5",
+      color: "#cf5b1e",
     },
     {
-      title: 'JavaScript',
-      icon: 'fab fa-js-square',
-      color: '#b2b200'
+      title: "CSS",
+      icon: "fab fa-css3",
+      color: "#559cc4",
     },
-    {
-      title: 'HTML',
-      icon: 'fab fa-html5',
-      color: '#cf5b1e'
-    },
-    {
-      title: 'CSS',
-      icon: 'fab fa-css3',
-      color: '#559cc4'
-    }
   ],
   technology: [
     {
-      title: 'React Js',
-      icon: 'fab fa-react',
-      color: '#00ff22'
+      title: "AWS",
+      icon: "fab fa-aws",
+      color: "#09041aff",
     },
     {
-      title: 'Angular',
-      icon: 'fab fa-angular',
-      color: '#00ff22'
+      title: "Node.js",
+      icon: "fab fa-node",
+      color: "#36d84cff",
     },
     {
-      title: 'Node.js',
-      icon: 'fab fa-node',
-      color: '#00ff22'
+      title: "React JS",
+      icon: "fab fa-react",
+      color: "#1a133aff",
     },
     {
-      title: 'Flask',
-      icon: 'fab fa-python',
-      color: '#32e0c4'
+      title: "Angular",
+      icon: "fab fa-angular",
+      color: "#00ff22",
+    },
+    {
+      title: "Spring Boot",
+      icon: "fab fa-java",
+      color: "#9296dbff",
+    },
+    {
+      title: "Apache Ant/Ivy",
+      icon: "fab fa-java",
+      color: "#9296dbff",
+    },
+    {
+      title: "Flask",
+      icon: "fab fa-python",
+      color: "#32e0c4",
     },
   ],
   database: [
     {
-      title: 'SQL',
-      icon: 'fa fa-database',
-      color: '#6685e4'
+      title: "SQL",
+      icon: "fa fa-database",
+      color: "#6685e4",
     },
     {
-      title: 'MongoDB',
-      icon: 'fa fa-database',
-      color: '#046e16'
+      title: "MongoDB",
+      icon: "fa fa-database",
+      color: "#046e16",
     },
     {
-      title: 'Firebase',
-      icon: 'fa fa-fire',
-      color: '#FFA000'
+      title: "Firebase",
+      icon: "fa fa-fire",
+      color: "#FFA000",
     },
   ],
   tools: [
     {
-      title: 'Git',
-      icon: 'fab fa-git-square',
-      color: '#1f1f1f'
+      title: "Git",
+      icon: "fab fa-git-square",
+      color: "#1f1f1f",
     },
     {
-      title: 'Github',
-      icon: 'fab fa-github-square',
-      color: '#1f1f1f'
+      title: "Github",
+      icon: "fab fa-github-square",
+      color: "#1f1f1f",
     },
     {
-      title: 'Firebase',
-      icon: 'fa fa-fire',
-      color: '#FFA000'
+      title: "Firebase",
+      icon: "fa fa-fire",
+      color: "#FFA000",
     },
   ],
   softskill: [
     {
-      type: 'img',
-      title: 'Communication skill',
-      icon: 'images/commskill2.svg',
-      color: '#1f1f1f'
+      type: "img",
+      title: "Communication skill",
+      icon: "images/commskill2.svg",
+      color: "#1f1f1f",
     },
     {
-      type: 'img',
-      title: 'Project planning <br/>&<br/> management',
-      icon: 'images/projectplan.svg',
-      color: '#1f1f1f'
+      type: "img",
+      title: "Project planning <br/>&<br/> management",
+      icon: "images/projectplan.svg",
+      color: "#1f1f1f",
     },
     {
-      title: 'Working in team',
-      icon: 'fa fa-users',
-      color: '#1f1f1f'
+      title: "Working in team",
+      icon: "fa fa-users",
+      color: "#1f1f1f",
     },
-  ]
-}
+  ],
+};
 
 export const projects = [
   {
     projectName: "FACIAL EMOTION DETECTION using Deep Learning",
     image: "images/fed.png",
-    summary:
-      `Emotion detection is the process of identifying human 
+    summary: `Emotion detection is the process of identifying human 
       emotions and emotional states using technology.
       this technology has found applications across 
       various industries, including customer 
@@ -122,8 +149,7 @@ export const projects = [
   {
     projectName: "IMAGE CAPTION GENERATION using Deep Learning",
     image: "images/capgen.png",
-    summary:
-      `Image caption generator is a process of
+    summary: `Image caption generator is a process of
         recognizing the context of an image and
         annotating it with relevant captions using deep
         learning, and computer vision.
@@ -134,8 +160,7 @@ export const projects = [
   {
     projectName: "Olama Council Trust Website",
     image: "images/council.png",
-    summary:
-      `Develop and deploy the olama council trust
+    summary: `Develop and deploy the olama council trust
         website.Here, I used my skills in front-end 
         technologies to design and build attractive,
          easy-to-use websites that met the trust's requirements. `,
@@ -161,8 +186,7 @@ export const projects = [
   {
     projectName: "Rich Text Editor",
     image: "images/editor.png",
-    summary:
-      "Browser based editor where text can be edit in rich format.",
+    summary: "Browser based editor where text can be edit in rich format.",
     preview: "https://notes-editor.azurewebsites.net/",
     techStack: ["React Js", "HTML", "CSS"],
   },
@@ -171,27 +195,26 @@ export const externalLinks = [
   {
     text: "GitHub",
     color: "black",
-    link: "https://github.com/maishtiyaque"
+    link: "https://github.com/maishtiyaque",
   },
   {
     text: "Codechef",
     color: "firebrick",
-    link: "https://www.codechef.com/users/ishtiyaque4755"
+    link: "https://www.codechef.com/users/ishtiyaque4755",
   },
   {
     text: "Leetcode",
     color: "orange",
-    link: "https://leetcode.com/maishtiyaque/"
+    link: "https://leetcode.com/maishtiyaque/",
   },
   {
     text: "Geekforgeeks",
     color: "darkgreen",
-    link: "https://auth.geeksforgeeks.org/user/ishtiyaque4755"
+    link: "https://auth.geeksforgeeks.org/user/ishtiyaque4755",
   },
   {
     text: "HackerRank",
     color: "forestgreen",
-    link: "https://www.hackerrank.com/IshtiyaqueAhmad"
+    link: "https://www.hackerrank.com/IshtiyaqueAhmad",
   },
 ];
-
